606473ea6cd95e1200c8665b2664f0cc985cf3d5
Ed Kozlowski <ekozlowski1@gmail.com>
2021-12-02 00:37:35 -0600

    Part 2 solved in JS

--- dec01/node/part2.js
+++ dec01/node/part2.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 var os = require('os');
 
 fs.readFile('../input.txt', 'utf-8', (err, data) => {
-    let count = 0
-    let old = null
-    let index = 0
     if (err) {
         console.error(err)
         return
     }
+    let count = 0
+    let index = 0
+
     data = data.split(os.EOL)
 
     let a = 0
@@ -16,23 +16,17 @@ fs.readFile('../input.txt', 'utf-8', (err, data) => {
     let c = 0
     let d = 0
 
-    while (index + 3 < length(data)) {
-        data
-        {
+    while (index + 3 < data.length) {
+        a = parseInt(data[index])
+        b = parseInt(data[index + 1])
+        c = parseInt(data[index + 2])
+        d = parseInt(data[index + 3])
+        if ((a+b+c) < (b+c+d)) {
+            count++
         }
+        console.log("Processed", d, "at index", index)
         index++
     }
 
-    data.forEach(element => {
-        element = parseInt(element)
-        if (old == null) {
-            old = element
-        } else {
-            if (element > old) {
-                count = count + 1
-            }
-            old = element
-        }
-    })
-    console.log("There are", count, "depth increases")
+    console.log("There are", count, "depth increases across 3 element ranges.")
 })
