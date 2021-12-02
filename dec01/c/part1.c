4308257e5ab665b14dd7324620034a14836c5976
Ed Kozlowski <ekozlowski1@gmail.com>
2021-12-02 01:32:22 -0600

    Day 1, Part 1 solved in C.

--- dec01/c/part1.c
+++ dec01/c/part1.c
@@ -13,21 +13,14 @@ int main()
     data d = {0, 0};
     int current = 0;
     fptr = fopen("../input.txt", "rb");
-    fseek(fptr, 0, SEEK_END);
-    long fsize = ftell(fptr);
-    rewind(fptr);
-    printf("File is %ld long\n", fsize);
     char line [1000];
     while(fgets(line, sizeof line, fptr) != NULL) {
         current = atoi(line);
-        printf("d.last is: %d\n", d.last);
-        printf("Current is %d\n", current);
         if (d.last == 0) {
             d.last = current;
             continue;
         } else {
             if (current > d.last) {
-                printf("%d is greater than %d", current, d.last);
                 d.depth_increase_count++;
             }
         }
