4df040735e142b2b52baa5e4b5ebc4480ab763ab
Ed Kozlowski <ekozlowski1@gmail.com>
2021-12-04 00:02:01 -0600

    Fun with Coroutines in Day 1 - part 2 in Python.

--- dec01/py/part2.py
+++ dec01/py/part2.py
@@ -1,11 +1,33 @@
-depths = [int(x.strip()) for x in open('../input.txt', 'r').readlines()]
+from collections import deque
+depths = deque([int(x.strip()) for x in open('../input.txt', 'r').readlines()])
 
-increases = 0
-for i, d in enumerate(depths):
-    if i + 4 > len(depths):
-        break
-    first_window = depths[i:i + 3]
-    second_window = depths[i + 1:i + 4]
-    if sum(first_window) < sum(second_window):
-        increases += 1
-print(increases)
+
+def process(stream, next_coroutine):
+    while(len(stream) >= 3):
+        vals = stream[0], stream[1], stream[2]
+        next_coroutine.send(vals)
+        stream.popleft()
+    next_coroutine.close()
+
+
+def get_depth_increases():
+    increases = 0
+    previous = None
+    try:
+        while True:
+            current = (yield)
+            if previous is None:
+                previous = current
+                continue # can't compare b/c we don't have a previous value.
+            else:
+                if sum(current) > sum(previous):
+                    increases += 1
+            previous = current
+    except GeneratorExit:
+        print("Done with depth increases!")
+        print(f"There were {increases} depth increases")
+
+
+gdi = get_depth_increases()
+gdi.__next__()  # initialize coroutine (run to 'current = (yield)')
+process(depths, gdi)
